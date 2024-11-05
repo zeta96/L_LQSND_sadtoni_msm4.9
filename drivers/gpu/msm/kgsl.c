d99e62446f190068b78dcd4445ba065df395aee4
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-11-06 06:07:13 +0700

    msm: kgsl: Set IRQF_NOBALANCING only for kgsl_3d0_irq
    skip irq balancing, kang from kerneltoast/android_kernel_google_gs201@51af002 and https://github.com/kdrag0n/proton_kernel_redbull/commit/56b067ba5ec885c783336317c0b769fb887c766a

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4756,6 +4756,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
+	unsigned long irqflags = IRQF_TRIGGER_HIGH;
 
 	status = _register_device(device);
 	if (status)
@@ -4824,8 +4825,12 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_pwrctrl_close;
 	}
 
+	if (!strcmp(device->name, "kgsl_3d0_irq")) {
+		irqflags |= IRQF_NOBALANCING;
+	}
+
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				  kgsl_irq_handler, IRQF_TRIGGER_HIGH,
+				  kgsl_irq_handler, irqflags, 
 				  device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
